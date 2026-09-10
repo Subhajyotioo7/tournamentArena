3e590bfc46926cdefe8e18fc42c0348f6cc52b62
Subhajyotioo7 <programingsubhajyoti@gmail.com>
2026-09-10 11:03:27 +0000

    fix scan 5

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -206,7 +206,7 @@ SIMPLE_JWT = {
     'USER_ID_FIELD': 'id',
     'USER_ID_CLAIM': 'user_id',
     'AUTH_TOKEN_CLASSES': ('rest_framework_simplejwt.tokens.AccessToken',),
-    'TOKEN_TYPE_CLAIM': 'token_type',  # nosec B105
+    'TOKEN_TYPE_CLAIM': 'token_type',
 }
 
 CSRF_TRUSTED_ORIGINS = [

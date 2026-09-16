85b57a24a9f766bf9cb8c2f6508ae9bec34a1667
Subhajyotioo7 <programingsubhajyoti@gmail.com>
2026-09-16 06:08:02 +0000

    dockerfile2

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -130,7 +130,7 @@ else:
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.environ.get('DJANGO_DB_PATH', str(BASE_DIR / 'db.sqlite3')),
     }
 }
 
